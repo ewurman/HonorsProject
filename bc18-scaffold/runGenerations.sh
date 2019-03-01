91bed203ee3b90ab9eff7cdb96f91434ca6c9add
ewurman <ewurman@gmail.com>
2019-02-28 20:31:23 -0500

    remove dependency download

--- bc18-scaffold/runGenerations.sh
+++ bc18-scaffold/runGenerations.sh
@@ -18,16 +18,16 @@ fi
 
 echo "=== STARTING THE MANAGER (no docker) ==="
 
-echo "=== ensuring dependencies ==="
-mtput setaf 5
-echo "$ pip3 install --user cffi eel tqdm werkzeug psutil requests"
-mtput sgr0
-pip3 install --user cffi eel tqdm werkzeug psutil requests
-RESULT=$?
-if [ $RESULT -ne 0 ]; then
-echo "Warning: pip install failed!"
-echo "I'll keep going, but maybe try to fix whatever error you just got."
-fi
+#echo "=== ensuring dependencies ==="
+#mtput setaf 5
+#echo "$ pip3 install --user cffi eel tqdm werkzeug psutil requests"
+#mtput sgr0
+#pip3 install --user cffi eel tqdm werkzeug psutil requests
+#RESULT=$?
+#if [ $RESULT -ne 0 ]; then
+#echo "Warning: pip install failed!"
+#echo "I'll keep going, but maybe try to fix whatever error you just got."
+#fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH="$DIR/battlecode/python:$PYTHONPATH"
